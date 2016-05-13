413b761a4f3b90273778ac49880ea5d33f072aaf
HWliao <499606788@qq.com>
2016-05-13 18:39:30 +0800

    demo1

--- shiro/shiroDemo1/src/test/java/com/example/ShiroDemo1ApplicationTests.java
+++ shiro/shiroDemo1/src/test/java/com/example/ShiroDemo1ApplicationTests.java
@@ -1,5 +1,13 @@
 package com.example;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -10,7 +18,26 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class ShiroDemo1ApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void testShiro1() {
+		// 1.获取security manager工厂,此处使用user.ini初始化
+		// 2.得到securityManager实例 并绑定给securityUtils
+		// 3.得到subject token
+		// 4.登入
+		// 5.身份验证失败
+		// 6.退出
+		Factory<SecurityManager> factory = new IniSecurityManagerFactory(
+				"classpath:data/user/user.ini");
+		SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+		try {
+			subject.login(token);
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+		}
+		Assert.assertEquals(true, subject.isAuthenticated()); // 断言用户已经登录
+		subject.logout();
 	}
 
 }
