b58a001ad20f8f69bc567daecc650005c356aa03
HWliao <499606788@qq.com>
2016-05-13 11:33:56 +0800

    shiro

---
+++ shiro/shiroDemo1/src/main/java/com/example/ShiroDemo1Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroDemo1Application.class, args);
+	}
+}
---
+++ shiro/shiroDemo1/src/test/java/com/example/ShiroDemo1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ShiroDemo1Application.class)
+public class ShiroDemo1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
