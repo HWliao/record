51a5183cc35003359bb73b9af8c41be04708fddc
HWliao <499606788@qq.com>
2016-09-22 11:14:01 +0800

    add spring-boot demo server

--- nim/src/index.js
+++ nim/src/index.js
@@ -1,6 +1,19 @@
 /**
  * Created by lenovo on 2016/9/19.
  */
-function test1(a, b) {
-  console.log(123);
+
+var data = {};
+var nim = NIM.getInstance({
+  // debug: true,
+  appKey: 'appKey',
+  account: 'account',
+  token: 'token',
+  onconnect: onConnect,
+  onwillreconnect: onWillReconnect,
+  ondisconnect: onDisconnect,
+  onerror: onError
+});
+
+function onConnect() {
+  console.log('');
 }
